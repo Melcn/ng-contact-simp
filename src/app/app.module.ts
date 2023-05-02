398a1aeaddbedb5a1b14f3107380a31dbc95c7c0
Melcn <melaniearinosy@outlook.com>
2023-05-02 10:17:11 +0200

    generate header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { SelectedContactComponent } from './selected-contact/selected-contact.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
-    SelectedContactComponent
+    SelectedContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
