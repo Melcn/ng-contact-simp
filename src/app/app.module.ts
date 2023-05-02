c603eb7873a2573ea6f248f4c2a7fd3c2a687935
Melcn <melaniearinosy@outlook.com>
2023-05-02 10:16:34 +0200

    generate selected contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { SelectedContactComponent } from './selected-contact/selected-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactListComponent
+    ContactListComponent,
+    SelectedContactComponent
   ],
   imports: [
     BrowserModule
